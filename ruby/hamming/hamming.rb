e3cfaacabba0f2d57b2342ab742f055196fea216
cocoswank <patchkatie@gmail.com>
2015-02-10 17:37:59 -0600

    Initial commit, completed hamming

---
+++ ruby/hamming/hamming.rb
@@ -0,0 +1,21 @@
+class Hamming
+
+  def self.compute(a, b)
+
+    hamming_distance = 0
+    i = 0
+
+      until i == a.length
+
+        if a[i] != b[i]
+        hamming_distance += 1
+        end
+
+      i += 1
+      end
+
+    return hamming_distance
+
+  end
+end
+
